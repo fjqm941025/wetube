e0b30e26f41cfc6df85b560445620e812dea37d8
fjqm941025 <fjqm941025@naver.com>
2021-01-11 16:03:26 +0900

    2.21 Home Controller:

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 12325,
+    title: "video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 2504,
+      name: "Hyesun",
+      email: "fjqm941025@naver.com",
+    },
+  },
+  {
+    id: 3948,
+    title: "Hello Video",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 4504,
+      name: "Hyesun",
+      email: "fjqm941025@naver.com",
+    },
+  },
+  {
+    id: 463423,
+    title: "OMG",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 104,
+      name: "Hyesun",
+      email: "fjqm941025@naver.com",
+    },
+  },
+];
