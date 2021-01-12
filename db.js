b2e88e1542e584609913fef8d1121e6b4d88ed48
fjqm941025 <fjqm941025@naver.com>
2021-01-12 20:54:13 +0900

    2.25 More Controlers

--- db.js
+++ db.js
@@ -1,41 +1,15 @@
-export const videos = [
-  {
-    id: 12325,
-    title: "video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/details/https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 2504,
-      name: "Hyesun",
-      email: "fjqm941025@naver.com",
-    },
-  },
-  {
-    id: 3948,
-    title: "Hello Video",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 4504,
-      name: "Hyesun",
-      email: "fjqm941025@naver.com",
-    },
-  },
-  {
-    id: 463423,
-    title: "OMG",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 104,
-      name: "Hyesun",
-      email: "fjqm941025@naver.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
