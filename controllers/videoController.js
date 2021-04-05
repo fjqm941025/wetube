658b93414c642e8481439d80c88b9d9640166ad6
hyesunie <hyesunie1025@gmail.com>
2021-04-06 07:26:31 +0900

    11.5 Conclusions

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
-import { Collection } from "mongoose";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   try {
@@ -62,7 +62,9 @@ export const videoDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const video = await Video.findById(id).populate("creator");
+    const video = await Video.findById(id)
+      .populate("creator")
+      .populate("comments");
 
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
@@ -133,6 +135,30 @@ export const postRegisterView = async (req, res) => {
     video.views += 1;
     video.save();
     res.status(200);
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
+
+// Add Comment
+
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: req.user.id,
+    });
+    video.comments.push(newComment.id);
+    video.save();
+    req.user.comments.push(newComment.id);
+    req.user.save();
   } catch (error) {
     res.status(400);
   } finally {
